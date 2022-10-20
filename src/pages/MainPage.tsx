53dbc2692fbd2f40dd432fa758819858c3c5700a
Grekova Elena <grekovalena01@gmail.com>
2022-10-20 22:40:53 +0500

    feat(Main page): add professions block

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Intro, About, StudentReview, Advantages } from '../components';
+import { Intro, About, StudentReview, Advantages, Professions } from '../components';
 
 type MainPageProps = {};
 
 export const MainPage: React.FC<MainPageProps> = () => {
 	return (
-			<section style={{ paddingBottom: 128 }}>
-				<Intro />
-				<About />
-        <Advantages />
-				<StudentReview />
-			</section>
+		<section style={{ paddingBottom: 128 }}>
+			<Intro />
+			<About />
+			<Advantages />
+			<StudentReview />
+			<Professions />
+		</section>
 	);
 };
