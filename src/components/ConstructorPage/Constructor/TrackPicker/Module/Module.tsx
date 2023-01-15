e52632360051f07b6f70016dc982aab04b7d9ec4
Grekova Elena <grekovalena01@gmail.com>
2023-01-15 15:59:28 +0500

    feat(constructor): add auto collapse module when course was selected

--- src/components/ConstructorPage/Constructor/TrackPicker/Module/Module.tsx
+++ src/components/ConstructorPage/Constructor/TrackPicker/Module/Module.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../Card/Card';
 import { EducationModule } from '../../../../../common/types';
 import { Droppable } from 'react-beautiful-dnd';
 import './Module.scss';
 import { Column } from '../../../Context/types';
 import { Collapse } from 'antd';
+import { useConstructorContext } from '../../../Context';
 
 const { Panel } = Collapse;
 
@@ -14,6 +15,23 @@ type ModuleProps = {
 };
 
 export const Module: React.FC<ModuleProps> = ({ module, column }) => {
+	const {
+		state: { semesters, columns },
+	} = useConstructorContext();
+
+	const [isModuleCollapse, setIsModuleCollapse] = useState<string[] | string>(
+		!module.is_spec ? '' : [`${module.id}`]
+	);
+
+	useEffect(() => {
+		if (!module.is_spec) {
+			return;
+		}
+		const moduleId = column.id === 2 ? module.id : `module_${module.id}`;
+		const choiceList = columns['2']?.items.find((item) => item.id === moduleId);
+		setIsModuleCollapse(choiceList?.choice_limit === choiceList?.disciplines.length ? '' : [`${module.id}`]);
+	}, [semesters]);
+
 	return column.id === 1 && !module.is_spec ? null : (
 		<Droppable
 			droppableId={module.id.toString()}
@@ -23,7 +41,11 @@ export const Module: React.FC<ModuleProps> = ({ module, column }) => {
 			{(provided) => {
 				return (
 					<div className="module_wrapper" {...provided.droppableProps} ref={provided.innerRef}>
-						<Collapse ghost defaultActiveKey={module.is_spec ? [`${module.id}`] : ''}>
+						<Collapse
+							ghost
+							activeKey={isModuleCollapse}
+							onChange={() => setIsModuleCollapse((prev) => (prev === '' ? [`${module.id}`] : ''))}
+						>
 							<Panel
 								header={module.title}
 								key={module.id}
