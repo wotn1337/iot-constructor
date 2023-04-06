57f213453e8aeb3414bd9dddc74e96fd7b356b82
Белов Артём <artem.belov.0320@gmail.com>
2023-04-06 20:14:45 +0500

    feat(professions): expandable filters

--- src/components/common/FilterableContent/FiltersList/FiltersList.tsx
+++ src/components/common/FilterableContent/FiltersList/FiltersList.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Filter } from '../types';
-import { Checkbox, Divider, Space } from 'antd';
+import { Button, Checkbox, Divider, Empty, Input, Space } from 'antd';
 import './FiltersList.scss';
 import { Id } from '../../../../common/types';
 import { Loader } from '../../Loader/Loader';
+import { PlusOutlined } from '@ant-design/icons';
 
 type FiltersListProps = {
 	filter: Filter;
@@ -11,30 +12,60 @@ type FiltersListProps = {
 };
 
 export const FiltersList: React.FC<FiltersListProps> = ({ filter, divider }) => {
-	const { title, items, selectedIds, onChange, loading } = filter;
+	const { title, selectedIds, onChange, loading } = filter;
+	const [expanded, setExpanded] = useState(false);
+	const [searchValue, setSearchValue] = useState<string>();
+
+	const items = useMemo(() => {
+		if (!expanded) {
+			return filter.items.slice(0, 5);
+		}
+
+		return searchValue !== undefined
+			? filter.items.filter((item) => item.title.toLowerCase().includes(searchValue?.toLowerCase()))
+			: [...filter.items];
+	}, [expanded, filter.items, searchValue]);
 
 	const handleOnChange = (id: Id) => {
 		const newIds = selectedIds.includes(id) ? selectedIds.filter((item) => item !== id) : [...selectedIds, id];
 		onChange(newIds);
 	};
 
+	useEffect(() => {
+		if (!expanded) {
+			setSearchValue(undefined);
+		}
+	}, [expanded]);
+
 	return (
 		<>
 			<Space direction="vertical" size={16} className="filters-list">
 				<span className="filters-list__title">{title}</span>
 				<Loader loading={loading}>
-					<Space direction="vertical" size={10}>
-						{items.map((item) => (
-							<Checkbox
-								indeterminate={selectedIds.includes(item.id)}
-								onChange={() => handleOnChange(item.id)}
-								key={item.id}
-								className="filters-list__checkbox"
-								checked={false}
-							>
-								<span className="filters-list__item-title">{item.title}</span>
-							</Checkbox>
-						))}
+					<Space direction="vertical" size={10} className="filters-list__inner-space">
+						{expanded && <Input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />}
+						{items.length ? (
+							<Space direction="vertical" size={10} className="filters-list__items">
+								{items.map((item) => (
+									<Checkbox
+										indeterminate={selectedIds.includes(item.id)}
+										onChange={() => handleOnChange(item.id)}
+										key={item.id}
+										className="filters-list__checkbox"
+										checked={false}
+									>
+										<span className="filters-list__item-title">{item.title}</span>
+									</Checkbox>
+								))}
+							</Space>
+						) : (
+							<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Ничего не найдено" />
+						)}
+						{filter.items.length > 5 && !expanded && (
+							<Button type="link" className="filters-list__more-button" onClick={() => setExpanded(true)}>
+								Еще {filter.items.length - 5} <PlusOutlined />
+							</Button>
+						)}
 					</Space>
 				</Loader>
 			</Space>
